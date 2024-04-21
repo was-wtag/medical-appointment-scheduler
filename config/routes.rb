4afb6c178aabd0ab66862fd91674f8104494caf7
Mohammad <sameenwasee@gmail.com>
2024-04-21 13:56:56 +0600

    Add home routes
    - Added HomeController
    - Added three routes for index, login, and signup
    - Added default views for all the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
+
+  # Home
+  get 'home/index'
+  get 'home/login'
+  get 'home/signup'
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root to: redirect('/home/index')
 end
