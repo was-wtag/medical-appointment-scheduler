728858e01b674af9e5feb2c9fff8fd6bc91d6be3
Mohammad Samin-Al-Wasee <samin.al_wasee@welldev.io>
2024-04-24 13:23:37 +0600

    Fix format error when email or phone number is blank

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,9 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true, length: { maximum: 128 }
   validates :last_name, presence: true, length: { maximum: 128 }
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :phone_number, presence: true, uniqueness: true, format: { with: /\A\+?[0-9]{1,3}-?[0-9]{1,14}\z/ }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, allow_blank: true }
+  validates :phone_number, presence: true, uniqueness: true,
+                           format: { with: /\A\+?[0-9]{1,3}-?[0-9]{1,14}\z/, allow_blank: true }
   validates :password, length: { minimum: 8 }, if: -> { new_record? || password.present? }
 
   def generate_confirmation_token
