41a6fc50373a55d46510c041c9543e34944d9e14
Mohammad Samin-Al-Wasee <samin.al_wasee@welldev.io>
2024-04-24 19:57:38 +0600

    Refactored users and confirmations controllers

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,36 +1,64 @@
 # frozen_string_literal: true
 
 class ConfirmationsController < ApplicationController
+  before_action :set_action_to_user_finder, :set_action_to_user_args, only: %i[show create]
+  before_action only: %i[show create] do
+    set_user { |finder, args| finder.call(*args[:args], **args[:kwargs]) }
+  end
+
   def show
-    confirmation_token = params[:token]
-    user = User.find_signed(confirmation_token, purpose: :account_confirmation)
+    return flash[:alert] = 'Account confirmation failed' if @user.nil?
+    return flash[:notice] = 'Account already confirmed' if @user.active?
 
-    flash[:alert] = 'Account confirmation failed' if user.nil?
-    flash[:notice] = 'Account already confirmed' if user&.active?
-    flash[:notice] = 'Account confirmation successful' if user&.pending?
+    return unless @user.pending?
 
-    user&.active!
+    @user.active!
+    flash[:notice] = 'Account confirmation successful'
   end
 
   def new; end
 
   def create
-    user = User.find_by(email: params[:email])
+    return redirect_to_new_confirmation('Account can not be confirmed', :alert) if @user.nil?
+    return redirect_to_new_confirmation('Account already confirmed', :alert) if @user.active?
 
-    flash[:alert] = 'Account can not be confirmed' if user.nil?
-    flash[:alert] = 'Account already confirmed' if user&.active?
-    flash[:notice] = 'Confirmation email sent' if user&.pending?
+    return unless @user.pending?
 
-    redirect_to new_confirmation_url
-    return unless user&.pending?
-
-    user.generate_confirmation_token
-    user.send_confirmation_email
+    resend_confirmation
+    redirect_to_new_confirmation('Confirmation email sent', :notice)
   end
 
   private
 
-  def confirmation_params
-    params.require(:confirmation).permit(:email)
+  def resend_confirmation
+    @user.generate_confirmation_token
+    @user.send_confirmation_email
+    flash[:notice] = 'Confirmation email sent'
+  end
+
+  def redirect_to_new_confirmation(message, type)
+    flash[type] = message
+    redirect_to new_confirmation_url
+  end
+
+  def set_user
+    finder = @action_to_user_finder[action_name.to_sym]
+    args = @action_to_user_args[action_name.to_sym]
+
+    @user = yield(finder, args)
+  end
+
+  def set_action_to_user_finder
+    @action_to_user_finder = {
+      show: User.method(:find_signed),
+      create: User.method(:find_by)
+    }
+  end
+
+  def set_action_to_user_args
+    @action_to_user_args = {
+      show: { args: [params[:token]], kwargs: { purpose: :account_confirmation } },
+      create: { args: [], kwargs: { email: params[:email] } }
+    }
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,40 +23,23 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html do
-          redirect_to user_url(@user), notice: 'Registration successful. Confirmation email has been sent.'
-        end
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    return render :new, status: :unprocessable_entity unless @user.save
+
+    redirect_to @user, notice: 'Registration successful. Confirmation email sent.'
   end
 
   # PATCH/PUT /users/1 or /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to user_url(@user), notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    return render :edit, status: :unprocessable_entity unless @user.update(user_params)
+
+    redirect_to @user, notice: 'User was successfully updated.'
   end
 
   # DELETE /users/1 or /users/1.json
   def destroy
     @user.destroy!
 
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to users_url, notice: 'User was successfully destroyed.'
   end
 
   private
