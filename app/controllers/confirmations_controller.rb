f58b65fc340aa081b2ef6664a1d14b861ee80cc1
Mohammad Samin-Al-Wasee <samin.al_wasee@welldev.io>
2024-04-25 09:59:54 +0600

    Fix multiple flash message during confirmation

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,31 +1,31 @@
 # frozen_string_literal: true
 
 class ConfirmationsController < ApplicationController
-  before_action :set_action_to_user_finder, :set_action_to_user_args, only: %i[show create]
+  before_action :set_action_to_user_finder, :set_action_to_user_args
   before_action only: %i[show create] do
     set_user { |finder, args| finder.call(*args[:args], **args[:kwargs]) }
   end
 
   def show
-    return flash[:alert] = 'Account confirmation failed' if @user.nil?
-    return flash[:notice] = 'Account already confirmed' if @user.active?
+    return flash.now[:alert] = 'Account confirmation failed' if @user.nil?
+    return flash.now[:notice] = 'Account already confirmed' if @user.active?
 
     return unless @user.pending?
 
     @user.active!
-    flash[:notice] = 'Account confirmation successful'
+    flash.now[:notice] = 'Account confirmation successful'
   end
 
   def new; end
 
   def create
-    return redirect_to_new_confirmation('Account can not be confirmed', :alert) if @user.nil?
-    return redirect_to_new_confirmation('Account already confirmed', :alert) if @user.active?
+    return redirect_to new_confirmation_url, alert: 'Account can not be confirmed' if @user.nil?
+    return redirect_to new_confirmation_url, alert: 'Account already confirmed' if @user.active?
 
     return unless @user.pending?
 
     resend_confirmation
-    redirect_to_new_confirmation('Confirmation email sent', :notice)
+    redirect_to new_confirmation_url, notice: 'Confirmation email sent'
   end
 
   private
@@ -33,12 +33,6 @@ class ConfirmationsController < ApplicationController
   def resend_confirmation
     @user.generate_confirmation_token
     @user.send_confirmation_email
-    flash[:notice] = 'Confirmation email sent'
-  end
-
-  def redirect_to_new_confirmation(message, type)
-    flash[type] = message
-    redirect_to new_confirmation_url
   end
 
   def set_user
