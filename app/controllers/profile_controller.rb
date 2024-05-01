c2ea85d6cf0bc43865a7c5a4f6f01579a4c0f7ec
Mohammad Samin-Al-Wasee <samin.al_wasee@welldev.io>
2024-05-01 17:34:05 +0600

    Add profile views and partials with controller logic for showing profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 class ProfileController < ApplicationController
+  include Authenticable
+
+  before_action :authenticate!
+  before_action -> { self.current_profile = current_user.profile }
+  before_action -> { self.current_profile_partial = current_user.doctor? ? 'doctor_profile' : 'patient_profile' }
+
+  attr_accessor :current_profile, :current_profile_partial
+
   def show; end
 
   def edit; end
