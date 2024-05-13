5cf711becaff6c81e365c0fe17e32fe3589742f8
Mohammad Samin-Al-Wasee <samin.al_wasee@welldev.io>
2024-05-13 13:44:23 +0600

    Add action, view and policy for admin dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,2 +1,24 @@
+# frozen_string_literal: true
+
 class DashboardController < ApplicationController
+  include Authenticable
+
+  before_action :authenticate!
+  before_action :authorize_dashboard
+
+  attr_accessor :users, :doctors, :appointments
+
+  def show
+    # Possible Refactor to use single db query
+    self.users = policy_scope(User, policy_scope_class: UserPolicy::Scope)
+    self.doctors = policy_scope(User, policy_scope_class: DoctorPolicy::Scope)
+    self.appointments = policy_scope(Appointment, policy_scope_class: AppointmentPolicy::Scope)
+    ##########################################
+  end
+
+  private
+
+  def authorize_dashboard
+    authorize :dummy, policy_class: DashboardPolicy
+  end
 end
