5fa6c28e615f81504f775add012e91622af50489
Mohammad Samin-Al-Wasee <samin.al_wasee@welldev.io>
2024-05-13 14:33:23 +0600

    Update root route redirection according to user role

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,26 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  def index; end
+  include UserSettable
+
+  before_action lambda {
+                  self.action_to_user_finder = {
+                    index: User.method(:find)
+                  }
+                },
+                lambda {
+                  token = cookies[:jwt]
+                  user_id = JwtService.decode(token)['user_id']
+                  self.action_to_user_args = {
+                    index: { args: [user_id], kwargs: {} }
+                  }
+                }, :set_user, only: %i[index]
+
+  def index
+    return unless cookies.key?(:jwt)
+
+    redirect_to user.admin? ? dashboard_url : profile_url
+  end
 
   def login
     redirect_to new_auth_url
